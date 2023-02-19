c7709a0368f803b33e74bb07564d534e059b17c2
marona42 <marona42@naver.com>
2023-02-19 14:41:03 +0900

    polish grammar

--- src/components/sections/home/hero-contents-description-component.tsx
+++ src/components/sections/home/hero-contents-description-component.tsx
@@ -18,12 +18,12 @@ interface descriptionType {
 export default function HeroContentsDescriptionComponent({lang}: props){
     let newDescription:descriptionType = {
         en: [
-            'Only adults aged 19 or older can participate and it will be held in Korean',
-            'Dates, locations and cost are subject to change. Confirmed information will be announced before regular registration opens.'
+            'Only adults aged 19 or older can participate in and the event will be held in Korean',
+            'Date, location and costs are subject to change. Confirmed information will be announced before regular registration opens.'
         ],
         ko: [
-            '원활한 행사 진행을 위하여 만 19세 이상 성인만 참여하실 수 있으며 한국어로 진행됩니다.',
-            '현재 사전 등록을 진행 중에 있으며 날짜, 장소, 참가비가 변경될 수 있습니다. 확정된 정보는 정규 등록이 시작되기 전 알려드리겠습니다.'
+            '원활한 행사 진행을 위하여 만 19세 이상 성인만 참여하실 수 있으며 행사는 한국어로 진행됩니다.',
+            '현재 사전 등록을 진행 중이며 날짜, 장소, 참가비가 변경될 수 있습니다. 확정된 정보는 정규 등록이 시작되기 전 알려드리겠습니다.'
         ]
     };
 
